7203829048aab3f4fc95f45a4d65a9ffc397e332
PincoPallino12 <PincoPallino12@users.noreply.github.com>
2021-01-10 17:41:07 +0100

    Create Database.php
    Aggiunta un nuovo file Database

---
+++ inc/Database.php
@@ -0,0 +1,10 @@
+<?php 
+//connessione al db
+$server = "localhost";
+$utente = "root";  //radice
+$password ="";
+$database = "Profilo";
+
+$connessione =mysqli_connect($server,$utente,$password,$database);
+
+?>
