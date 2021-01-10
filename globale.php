f8e555a015705d748d5452aafbb94b078df494b3
GPesaresi <gianmi16@gmail.com>
2021-01-10 17:04:13 +0100

    aggiunta di alcuni file per farlo funzionare

---
+++ globale.php
@@ -0,0 +1,183 @@
+<?php
+	// avvio della sessione utente
+	session_start();
+
+	// home page
+	$home = "/Tennis";
+
+	// nome utente, se vuoto si presume utente anonimo
+	if (isset($_SESSION['nomeutente'])) {
+		$nomeutente	= $_SESSION['nomeutente'];
+	} else {
+		$nomeutente = "";
+	}
+	
+	// se l'utente è loggato indica se è socio oppure no
+	if (isset($_SESSION['socio'])) {
+		if (intval($_SESSION['socio']) == 1)
+		{
+			$socio = true;
+		}
+		else
+		{
+			$socio = false;
+		}
+	} else {
+		$socio = false;
+	}
+
+	// classe usata per la visualizzazione della griglia delle prenotazioni storiche utente
+	class StoricoUtente
+	{
+		// attributi
+		public $dataPrenotazione;
+		public $numeroCampo;
+		public $orario;
+		public $nGiocatori;
+		public $luci;
+		public $riscaldamento;
+		public $costo;
+
+		//costruttore
+		public function __construct($dataPrenotazione,$numeroCampo,$orario,$nGiocatori,$luci,$riscaldamento,$costo)
+		{
+			$this->dataPrenotazione    = $dataPrenotazione;
+			$this->numeroCampo = $numeroCampo;
+			$this->orario     = $orario;
+			$this->nGiocatori     = $nGiocatori;
+			$this->luci     = $luci;
+			$this->riscaldamento     = $riscaldamento;
+			$this->costo     = $costo;
+		}
+
+		public function rigaStorico($u)
+		{
+			return
+				"<tr><td>".$u->dataPrenotazione."</td>"
+				   ."<td>".$u->numeroCampo."</td>"
+				   ."<td>".$u->orario."</td>"
+				   ."<td>".$u->nGiocatori."</td>"
+				   ."<td>".$u->luci."</td>"
+				   ."<td>".$u->riscaldamento."</td>"
+				   ."<td>".$u->costo."</td></tr>";
+		}
+	}
+	
+	// classe usata per la visualizzazione della griglia delle prenotazioni
+	class DettaglioPrenotazioni
+	{
+		// attributi
+		public $numeroCampo;
+		public $orarioInizio;
+		public $orarioFine;
+		public $orario = array();
+
+		//costruttore
+		public function __construct($numeroCampo, $inizio, $fine)
+		{
+			$this->numeroCampo = $numeroCampo;
+			$this->orarioInizio = $inizio;
+			$this->orarioFine = $fine;
+			for ($idx = $inizio; $idx <= $fine; $idx++)
+			{
+				$this->orario[] = 0; 
+			}
+		}
+
+		public function rigaPrenotazioni($u, $hide)
+		{
+			$buffer = "<tr><td>".$u->numeroCampo."</td>";
+			for ($idx = $u->orarioInizio; $idx <= $u->orarioFine; $idx++)
+			{
+				$disable = "";
+				if($hide) {
+					$disable = "disabled=\"disabled\"";
+				}
+				$tag = "<label><input ".$disable." name=\"oraprenotazione[]\" type=\"checkbox\" value=\"".$u->numeroCampo."-".$idx."\"></label>";
+				if ($u->orario[$idx - $u->orarioInizio] == 1)
+				{
+					$tag = "<span class=\"fas fa-user-check\"></span>";
+				}
+				$buffer = $buffer."<td>";
+				$buffer = $buffer.$tag;
+				$buffer = $buffer."</td>";
+			}
+			return $buffer."</tr>";
+		}
+
+		public function Prenota($ora)
+		{
+			$this->Set($ora, 1);
+		}
+
+		public function Set($ora, $flag)
+		{
+			for ($idx = $this->orarioInizio; $idx <= $this->orarioFine; $idx++)
+			{
+				if ($idx == $ora)
+				{
+					$this->orario[$idx - $this->orarioInizio] = $flag;
+					return;
+				}
+			}
+		}
+	}
+
+	// --------------------------------------------------------------------------------
+	// array con i dati da visualizzare nella pagina delle prenotazioni storiche utente
+	$storicoPrenotazioni = array();
+
+	// ----------------------------------------------------------------
+	// array con i dati da visualizzare nella pagina delle prenotazioni
+	$dettaglioPrenotazioni = array();
+
+	// --------------------------------------------
+	// variabili di configurazione dell'applicativo
+
+	// orario di inizio delle prenotazioni
+	$orarioInizio = 9;
+	// orario di fine delle prenotazioni
+	$orarioFine = 21; 
+	// costo orario delle prenotazioni
+	$costoPrenotazione = 1205;
+	// giorno iniziale delle prenotazioni da 0 a 6 : DOM ...
+	$giornoiniziale = 6;
+	// numero dei campi disponibili
+	$numerocampi = 10;
+
+	// ----------------------------------------------------------------
+	// array con le descrizioni dei giorni della settimana
+	$descrizioneGiorniSettimana = array();
+
+	// inserimento dati
+	$descrizioneGiorniSettimana[]= "DOM";
+	$descrizioneGiorniSettimana[]= "LUN";
+	$descrizioneGiorniSettimana[]= "MAR";
+	$descrizioneGiorniSettimana[]= "MER";
+	$descrizioneGiorniSettimana[]= "GIO";
+	$descrizioneGiorniSettimana[]= "VEN";
+	$descrizioneGiorniSettimana[]= "SAB";
+	$descrizioneGiorniSettimana[]= "DOM";
+	$descrizioneGiorniSettimana[]= "LUN";
+	$descrizioneGiorniSettimana[]= "MAR";
+	$descrizioneGiorniSettimana[]= "MER";
+	$descrizioneGiorniSettimana[]= "GIO";
+	$descrizioneGiorniSettimana[]= "VEN";
+	$descrizioneGiorniSettimana[]= "SAB";
+
+	$dataattuale = date("Ymd"); 
+	$giornoattuale = date("w");
+
+	// determina la data di inizio e di fine della settimana di prenotazioni del tabellone
+	$rangePrenotazioniInizio = $rangePrenotazioniFine = 0;
+	for ($delta = 0; $delta < 7; $delta++)
+	{
+		$offset = date('w',strtotime("-".$delta." day"));
+		if($offset == $giornoiniziale){
+			$rangePrenotazioniInizio = strtotime("-".$delta." day");
+			$rangePrenotazioniFine = strtotime("+6 day", $rangePrenotazioniInizio);
+			break;
+		}
+	}
+
+?>
--- index.php
+++ index.php
@@ -1,11 +1,106 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+    <title>Tennis</title>
+    <!-- Bootstrap core CSS -->
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="site.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include 'globale.php' ?>
+    <?php include 'inc/proc-index.php' ?>
+    <?php include 'menu.php' ?>
+
+    <main role="main" class="container container-form-signin">
+        <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+            <div class="col-md-6 px-0">
+                <h1 class="display-4 font-italic">Tennis</h1>
+                <p class="lead my-3">/</p>
+                <p class="lead mb-0"><a href="#" class="text-white font-weight-bold"></a></p>
+            </div>
+        </div>
+
+        <div class="row mb-2">
+            <div class="col-md-6">
+                <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                    <div class="card-body d-flex flex-column align-items-start">
+                        <strong class="d-inline-block mb-2 text-primary">/</strong>
+                        <h3 class="mb-0">
+                        </h3>
+                    </div>
+                    <img class="card-img-right flex-auto d-none d-md-block" src="/Tennis/img/campo1.jpg"
+                        alt="Card image cap">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                    <div class="card-body d-flex flex-column align-items-start">
+                        <strong class="d-inline-block mb-2 text-primary">/</strong>
+                        <h3 class="mb-0">
+                        </h3>
+                    </div>
+                    <img class="card-img-right flex-auto d-none d-md-block" src="/Tennis/img/campo2.jpg"
+                        alt="Card image cap">
+                </div>
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col-md-6">
+                <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                    <div class="card-body d-flex flex-column align-items-start">
+                        <strong class="d-inline-block mb-2 text-primary">/</strong>
+                        <h3 class="mb-0">
+                        </h3>
+                    </div>
+                    <img class="card-img-right flex-auto d-none d-md-block" src="/Tennis/img/campo3.jpg"
+                        alt="Card image cap">
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                    <div class="card-body d-flex flex-column align-items-start">
+                        <strong class="d-inline-block mb-2 text-primary">/</strong>
+                        <h3 class="mb-0">
+                        </h3>
+                    </div>
+                    <img class="card-img-right flex-auto d-none d-md-block" src="/Tennis/img/campo4.jpg"
+                        alt="Card image cap">
+                </div>
+            </div>
+        </div>
+        </div>
+
+    </main><!-- /.container -->
+
+    <!-- Bootstrap core JavaScript
+================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script>
+    window.jQuery || document.write('<script src="assets/js/vendor/jquery-slim.min.js"><\/script>')
+    </script>
+    <script src="assets/js/vendor/popper.min.js"></script>
+    <script src="dist/js/bootstrap.min.js"></script>
+    <script src="assets/js/vendor/holder.min.js"></script>
+    <script>
+    Holder.addTheme('thumb', {
+        bg: '#55595c',
+        fg: '#eceeef',
+        text: 'Thumbnail'
+    });
+    </script>
+    <?php include 'debug.php' ?>
+</body>
+
+</html>
