3a97c4dff81623c5d3f1e2622ecdd9f49d3c7b67
PincoPallino12 <PincoPallino12@users.noreply.github.com>
2021-01-10 17:22:58 +0100

    Update proc-registrati.php

--- inc/proc-registrati.php
+++ inc/proc-registrati.php
@@ -1,2 +1,17 @@
 <?php
+    $Database="CREATE DATABASE IF NOT EXISTS Profilo";
+    echo "Database creata con sucesso";
+
+    $TabRegistrati="CREATE TABLE IF NOT EXISTS TabRegistrati
+                    (
+                        Utente int AUTO_INCREMENT PRIMARY KEY,
+                        nome varchar(15),
+                        cognome varchar(15),
+                        email varchar(20),
+                        ntelefono varchar(20),
+                        passwordUtente varchar(20),
+                        domandaRec varchar(30),
+                        rispostaRec varchar(30)
+                    )";
+    echo "Creazione Tabella con sucesso";
 ?>
