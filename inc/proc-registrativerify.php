d13e28a180f7ae46004b86182a907167012f409a
PincoPallino12 <PincoPallino12@users.noreply.github.com>
2021-01-13 16:36:24 +0100

    Revert "Update proc-registrativerify.php"
    This reverts commit 3b28a9bf09577892cabc91dc1e881f6e04ee1675.

--- inc/proc-registrativerify.php
+++ inc/proc-registrativerify.php
@@ -13,7 +13,7 @@
     $fasciaOrario = "";
 
     // controllo che la pagina è proviene da un submit
-    /*if (isset($_POST["nome"])) $nome = $_POST['nome'];  
+    if (isset($_POST["nome"])) $nome = $_POST['nome'];  
     if (isset($_POST["cognome"])) $cognome = $_POST['cognome'];  
     if (isset($_POST["email"])) $email = $_POST['email'];  
     if (isset($_POST["numeroDiTelefono"])) $numeroDiTelefono = $_POST['numeroDiTelefono'];  
@@ -21,35 +21,7 @@
     if (isset($_POST["domandaRecuperoPassword"])) $domandaRecuperoPassword = $_POST['domandaRecuperoPassword'];  
     if (isset($_POST["rispostaRecuperoPassword"])) $rispostaRecuperoPassword = $_POST['rispostaRecuperoPassword'];  
     if (isset($_POST["manoUsata"])) $manoUsata = $_POST['manoUsata'];  
-    if (isset($_POST["fasciaOrario"])) $fasciaOrario = $_POST['fasciaOrario'];*/  
+    if (isset($_POST["fasciaOrario"])) $fasciaOrario = $_POST['fasciaOrario'];  
 
     // una volta recuperati tutti i dati dalla form andiamo a memorizzare nel db
-
-    if(isset($_POST['Registrati']))
-    {
-        $server = "localhost";
-        $utente = "root";
-        $password = "";
-
-        $databaseConnected = mysqli_connect($server, $utente, $password);
-
-        $commando="USE Profilo";
-        $res=mysqli_query($databaseConnected,$commando);
-
-        $nome=$_POST['nome'];
-        $cognome=$_POST['cognome'];
-        $email=$_POST['email'];
-        $numeroDiTelefono=$_POST['numeroDiTelefono'];
-        $password=$_POST['password'];
-        $domandaRecuperoPassword=$_POST['domandaRecuperoPassword'];
-        $rispostaRecuperoPassword=$_POST['rispostaRecuperoPassword'];
-        $manoUsata=$_POST['manoUsata'];
-        $fasciaOraria=$_POST['fasciaOraria'];
-
-        $sql="INSERT INTO TabRegistrati(nome,cognome,email,ntelefono,passwordUtente,domandaRec,rispostaRec,manoUsata,Orario) Values ('$nome','$cognome','$email','$numeroDiTelefono','$password','$domandaRecuperoPassword','$rispostaRecuperoPassword','$manoUsata','$fasciaOraria')";
-        $res=mysqli_query($databaseConnected,$sql);
-        if(!$res)
-        die('errore inserimento $sql'.mysqli_errno($databaseConnected));
-    }
-   
 ?>
