e1e0820ebbbeb27a5327b4ea5c06d186fce62c44
unknown <63281357+PincoPallino12@users.noreply.github.com>
2021-01-14 16:56:07 +0100

    Update proc-recuperopwd.php

--- inc/proc-recuperopwd.php
+++ inc/proc-recuperopwd.php
@@ -1,2 +1,14 @@
 <?php
+
+    $server = "localhost";
+    $utente = "root";
+    $password = "";
+    $db="Profilo";
+
+    $databaseConnected = mysqli_connect($server, $utente, $password, $db);
+    if(isset($_POST['Avanti']))
+
+    {
+        $sql="SELECT email FROM TabRegistrati WHERE email = $email";
+    }
 ?>
