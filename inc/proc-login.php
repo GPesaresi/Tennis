c60a3c60a1f5f35aa8c50e3a3e178cd09336ec36
unknown <63281357+PincoPallino12@users.noreply.github.com>
2021-01-14 15:25:29 +0100

    Update proc-login.php

--- inc/proc-login.php
+++ inc/proc-login.php
@@ -1,2 +1,11 @@
 <?php
+    
+    $email = "";
+    $password = "";
+    if(isset($_POST['Accedi']))
+    {
+        if (isset($_POST["email"])) $email = $_POST['email'];
+        if (isset($_POST["password"])) $password = $_POST['password'];
+    }
+
 ?>
