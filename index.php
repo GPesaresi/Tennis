cdae15a398f4c137bbc89a6ecf55b6af1117bb11
Oleksandr Sahaydak <poloneaum18@gmail.com>
2021-01-15 10:24:55 +0100

    correzione index.php
    descrizione tennis, campi

--- index.php
+++ index.php
@@ -23,7 +23,14 @@
         <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
             <div class="col-md-6 px-0">
                 <h1 class="display-4 font-italic">Tennis</h1>
-                <p class="lead my-3">/</p>
+                <p class="lead my-3">Il tennis è uno sport che vede opposti due giocatori 
+                (uno contro uno, incontro singolare) o quattro (due contro due, 
+                incontro di doppio) in un campo diviso in due metà da una rete alta circa un metro dal terreno.
+                I giocatori utilizzano una racchetta, come se fosse il prolungamento del braccio, 
+                al fine di colpire una palla. In passato le racchette avevano un telaio in legno, 
+                successivamente in leghe leggere, ora in grafite pura o mista a kevlar, fibra di vetro, 
+                tungsteno o basalto, con una corda monofilamento o multifilamento congiunta, in diversi punti del telaio, 
+                in modo da formare una rete.</p>
                 <p class="lead mb-0"><a href="#" class="text-white font-weight-bold"></a></p>
             </div>
         </div>
@@ -34,9 +41,10 @@
                     <div class="card-body d-flex flex-column align-items-start">
                         <strong class="d-inline-block mb-2 text-primary">/</strong>
                         <h3 class="mb-0">
+                        Campo cementato
                         </h3>
                     </div>
-                    <img class="card-img-right flex-auto d-none d-md-block" src="Progetto_Tennis/Tennis/img/campo1.jpg"
+                    <img class="card-img-right flex-auto d-none d-md-block" src="Tennis/img/campo1.jpg"
                         alt="Card image cap">
                 </div>
             </div>
@@ -45,9 +53,10 @@
                     <div class="card-body d-flex flex-column align-items-start">
                         <strong class="d-inline-block mb-2 text-primary">/</strong>
                         <h3 class="mb-0">
+                        Campo cementato
                         </h3>
                     </div>
-                    <img class="card-img-right flex-auto d-none d-md-block" src="Progetto_Tennis/Tennis/img/campo2.jpg"
+                    <img class="card-img-right flex-auto d-none d-md-block" src="Tennis/img/campo2.jpg"
                         alt="Card image cap">
                 </div>
             </div>
@@ -58,9 +67,10 @@
                     <div class="card-body d-flex flex-column align-items-start">
                         <strong class="d-inline-block mb-2 text-primary">/</strong>
                         <h3 class="mb-0">
+                        Campo naturale
                         </h3>
                     </div>
-                    <img class="card-img-right flex-auto d-none d-md-block" src="Progetto_Tennis/Tennis/img/campo3.jpg"
+                    <img class="card-img-right flex-auto d-none d-md-block" src="Tennis/img/campo3.jpg"
                         alt="Card image cap">
                 </div>
             </div>
@@ -70,9 +80,10 @@
                     <div class="card-body d-flex flex-column align-items-start">
                         <strong class="d-inline-block mb-2 text-primary">/</strong>
                         <h3 class="mb-0">
+                        Campo sintetico
                         </h3>
                     </div>
-                    <img class="card-img-right flex-auto d-none d-md-block" src="Progetto_Tennis/Tennis/img/campo4.jpg"
+                    <img class="card-img-right flex-auto d-none d-md-block" src="Tennis/img/campo4.jpg"
                         alt="Card image cap">
                 </div>
             </div>
